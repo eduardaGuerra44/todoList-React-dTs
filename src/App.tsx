8743c385ecae7765fcad3fbf716cd215e2c905b4
Eduarda Guerra <eduardaguerra44@gmail.com>
2023-04-04 21:50:40 -0300

    iniciando projeto todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// uma variável que pode ser modificada com o tempo
+import { useState } from 'react';
+// C de components 
+import * as C from './App.styles'
+import { Item } from './types/Item'
 
-function App() {
+
+
+
+
+
+
+
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão amanhã', done: false},
+    { id: 2, name: 'Comprar pão amanhã', done: false},
+    { id: 3, name: 'Comprar pão amanhã', done: false}
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+  <C.Container>
+    <C.Area>
+      <C.Header>Lista de Tarefas</C.Header>
+
+    {list.map((item, index)=>(
+      <div>...</div>
+    ))}
+
+    </C.Area>
+  </C.Container>
   );
 }
 
