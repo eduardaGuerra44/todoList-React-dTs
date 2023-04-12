f8661794ef1b37170bd349dd3d73160d5ee5bbbd
Eduarda Guerra <eduardaguerra44@gmail.com>
2023-04-12 10:56:09 -0300

    adicionando novos item na lista

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,24 @@ import { useState } from "react";
 import * as C from "./App.styles";
 import { Item } from "./types/Item";
 import { ListItem } from "./components/ListItem";
-
+import { AddArea } from './components/AddArea'
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
-    { id: 1, name: "Comprar pão amanhã", done: false },
-    { id: 2, name: "Estudar Matemática", done: true},
-    { id: 3, name: "lavar roupa", done: false },
+    // { id: 1, name: "Comprar pão amanhã", done:  },
+    // { id: 2, name: "Estudar Matemática", done: },
+    // { id: 3, name: "lavar roupa", done: false },
   ]);
+    // função que recebe uma string e adiciona na lista
+    const handleAddTask = (taskName: string) => {
+      let newList = [...list];
+      newList.push({
+        id: list.length + 1,
+        name: taskName,
+        done: false
+      })
+      setList(newList)
+    }
 
   return (
     <C.Container>
@@ -20,8 +30,8 @@ const App = () => {
         <C.Header>Lista de Tarefas</C.Header>
 
         {/* area de adicionar novas tarefas  */}
+          <AddArea onEnter={handleAddTask}/>
 
-        
         {list.map((item, index) => (
            <ListItem key={index} item={item}/>
         ))}
